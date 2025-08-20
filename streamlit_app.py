f470f665029d9279a0feeb85a7b4292edc17827e
오세영 <leenight5309@naver.com>
2025-08-20 02:50:39 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🏫 우리반 알림장")
+st.write("우리 반의 중요한 소식을 확인하세요!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
